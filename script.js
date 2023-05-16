6ba3c0ec34c8b6213cb97531ff9f09cdd0c1a9d0
brigazvi <37640970+brigazvi@users.noreply.github.com>
2023-05-16 14:39:02 +0300

    adding script.js file and initial players and avatars arrays

---
+++ script.js
@@ -0,0 +1,51 @@
+let playerCounter = 6;
+const avatars = [
+    "https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436178.jpg?w=826&t=st=1684236472~exp=1684237072~hmac=9896da988c23afd90461b81afa500ebbae4f5902364bc421edef2af72e41b68d",
+    "https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg?w=826&t=st=1684236425~exp=1684237025~hmac=2e6b4da25fd2a82024a7056d8b33fb33bd054f525c22a386db962141bde8a77c",
+    "https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436180.jpg?w=826&t=st=1684236472~exp=1684237072~hmac=84ea28dfef7f9c0991650b2baf211c85ff5f382a564996567ac4ab2186606a64",
+    "https://img.freepik.com/premium-psd/3d-illustration-person-with-glasses-bow_23-2149436205.jpg?w=826",
+    "https://img.freepik.com/free-psd/3d-illustration-person-with-glasses_23-2149436190.jpg?w=826&t=st=1684236474~exp=1684237074~hmac=baaa8fba96bf22fa5f3a5a704774c132da4925cc5db799c7f348a252a887431a",
+    "https://img.freepik.com/free-psd/3d-illustration-business-man-with-glasses_23-2149436194.jpg?w=826&t=st=1684236475~exp=1684237075~hmac=7acac1dca05cbd4b99696dc6b9fe7fa677f3de42dba06fce90a95e4328e01c1a",
+    "https://img.freepik.com/free-psd/3d-illustration-bald-person_23-2149436183.jpg?w=826&t=st=1684236478~exp=1684237078~hmac=0f59f3382d9b1f0ba1330e7546924b8aa2fa4a7b5850236c486af128badaf807",
+    "https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses-green-hair_23-2149436201.jpg?w=740&t=st=1684236480~exp=1684237080~hmac=44b52924f4bdd7d372a7d8aa4b6df2056db7e6f081c1cd81b5eff4681c4bf463",
+    "https://img.freepik.com/free-psd/3d-illustration-person-with-glasses_23-2149436185.jpg?w=826&t=st=1684236431~exp=1684237031~hmac=2f53d17b3322147784ccb4d7272876a3e39646b828b7dd049d7d3ec538faeb95"
+]
+
+const players = [
+    {
+        name: "yocheved",
+        id: 0,
+        avatarUrl: avatars[0],
+        scores:[3,5,12]
+    },
+    {
+        name: "jacob",
+        id: 1,
+        avatarUrl: avatars[1],
+        scores:[1,0,5]
+    },
+    {
+        name: "aric",
+        id: 2,
+        avatarUrl: avatars[2],
+        scores:[5,8,2]
+    },
+    {
+        name: "beni",
+        id: 3,
+        avatarUrl: avatars[3],
+        scores:[5,78,43]
+    },
+    {
+        name: "Obama",
+        id: 4,
+        avatarUrl: avatars[4],
+        scores:[7,4,8]
+    },
+    {
+        name: "Obama",
+        id: 5,
+        avatarUrl: avatars[5],
+        scores:[9,67,4]
+    }
+]
